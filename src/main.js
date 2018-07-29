509c96c2709e0308b34e3ee5211b1b7175acccc8
Carson Farmer <carson.farmer@gmail.com>
2018-07-28 23:40:09 -0700

    simplify code and use jquery

--- src/main.js
+++ src/main.js
@@ -1,65 +1,31 @@
 import 'babel-polyfill'
 import getIpfs from 'window.ipfs-fallback'
 import queryString from 'query-string'
-
-const buildError = (error) => {
-  const main = document.getElementById('main')
-  main.appendChild(document.createTextNode(error))
-}
+import $ from 'jquery'
 
 const buildPage = (json) => {
-  const main = document.getElementById('main')
-
-  // Add profile pic
-  const img = document.createElement('div')
-  img.style.backgroundImage = `url('${json.pic.url}')`
-  img.className = 'pic'
-  main.appendChild(img)
-
-  // Add profile information
-  const profileDiv = document.createElement('div')
-  profileDiv.className = 'profile'
+  // Add profile image
+  $('#image').css('background-image', `url('${json.pic.url}')`)
   // Add name
-  const h1 = document.createElement('h1')
-  const name = document.createTextNode(`${json.name.first} ${json.name.last}`)
-  h1.appendChild(name)
-  profileDiv.appendChild(h1)
-
+  $('#name').text(`${json.name.first} ${json.name.last}`)
   // Add job title and employer (only first)
   const work = json.work[0]
-  const h2 = document.createElement('h2')
-  const link = document.createElement('a')
-  link.href = work.url
-  link.appendChild(document.createTextNode(work.employer))
-  h2.appendChild(document.createTextNode(`${work.title} at `))
-  h2.appendChild(link)
-  profileDiv.appendChild(h2)
-
+  const link = $('<a>', { text: work.employer, href: work.url })
+  $('#title').html(`${work.title} at `).append(link)
   // Add bio
-  const bioDiv = document.createElement('div')
-  const bio = document.createTextNode(`${json.bio}`)
-  bioDiv.className = 'bio'
-  bioDiv.appendChild(bio)
-  profileDiv.appendChild(bioDiv)
-
+  $('#bio').text(`${json.bio}`)
   // Add social media links
-  const socialDiv = document.createElement('div')
-  socialDiv.id = 'social'
+  const social = $('#social')
   for (let item of json.social) {
-    const a = document.createElement('a')
-    a.className = 'social'
-    a.href = item.url
-    const icon = document.createElement('i')
-    if (item.service === 'website') {
-      icon.className = 'fas fa-globe'
-    } else {
-      icon.className = `fab fa-${item.service}`
-    }
-    a.appendChild(icon)
-    socialDiv.appendChild(a)
+    const isWebsite = item.service === 'website'
+    social.append(
+      $('<a>', { href: item.url, class: 'social' }).append(
+        $('<i>', {
+          class: isWebsite ? 'fas fa-globe' : `fab fa-${item.service}`
+        })
+      )
+    )
   }
-  profileDiv.appendChild(socialDiv)
-  main.appendChild(profileDiv)
 }
 
 const setup = async () => {
@@ -84,7 +50,6 @@ const setup = async () => {
     buildPage(json)
   } catch(err) {
     console.log(err)
-    buildError(err)
   }
 }
 setup()
